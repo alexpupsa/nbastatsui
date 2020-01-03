965b74a30f673eb223bd874d57b3bf5553b9a537
Alexandru Pupsa <pupsaa@live.com>
2020-01-03 19:07:56 +0200

    Fix calendar to start weeks on Monday

--- src/app/team-list/team-list.component.ts
+++ src/app/team-list/team-list.component.ts
@@ -25,9 +25,23 @@ export class TeamListComponent implements OnInit {
 
   isLoading: boolean;
 
+  calendarSettings: any;
+
   constructor(private http: HttpClient) { }
 
   ngOnInit() {
+    this.calendarSettings = {
+      firstDayOfWeek: 1,
+      dayNames: ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"],
+      dayNamesShort: ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"],
+      dayNamesMin: ["Su", "Mo", "Tu", "We", "Th", "Fr", "Sa"],
+      monthNames: ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"],
+      monthNamesShort: ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"],
+      today: 'Today',
+      clear: 'Clear',
+      dateFormat: 'dd/MM/yy',
+      weekHeader: 'Wk'
+    };
     this.games = [];
     this.date = this.getDate();
     this.countResults = 0;
